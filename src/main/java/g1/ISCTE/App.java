65d74d55e6e67212aeb1ef35d83ac2d197f257dc
miguel-arrf <mferreira.oeiras@gmail.com>
2021-03-13 16:13:26 +0000

    Initial commit

---
+++ src/main/java/g1/ISCTE/App.java
@@ -0,0 +1,13 @@
+package g1.ISCTE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
