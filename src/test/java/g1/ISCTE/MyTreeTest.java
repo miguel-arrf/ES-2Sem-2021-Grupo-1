232f1a407c44c77daf752d5764343ae3a4b89da0
miguel-arrf <mferreira.oeiras@gmail.com>
2021-05-04 21:03:09 +0100

    Updated AppStyleTest.java. Updated AppStyle.java. Removed is_God_Class and is_Long_Method MetricBlocks from the RuleEditor panel.
    Updated MyTree.java.
    Improved RuleFileManagerTest.java.
    
    Added some files to the resource folder to be able to run some of the tests.

---
+++ src/test/java/g1/ISCTE/MyTreeTest.java
@@ -0,0 +1,42 @@
+package g1.ISCTE;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+class MyTreeTest {
+	
+
+
+	@Test
+	void testGetScrollPane() throws URISyntaxException {
+		JFXPanel panel = new JFXPanel();
+
+		URL sqlScriptUrl = MyTree.class.getClassLoader().getResource("forTestPurposes");
+		
+		MyTree myTree = new MyTree();
+				
+		ScrollPane scrollPane = myTree.getScrollPane(new File(sqlScriptUrl.toURI()));
+		
+		VBox scrollPaneContent = (VBox) scrollPane.getContent();
+		
+		VBox firstItem = (VBox)  scrollPaneContent.getChildren().get(1);
+	
+		assertEquals(firstItem.getChildren().size(),5);
+	}
+
+}
